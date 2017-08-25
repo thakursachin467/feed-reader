4a9710e46f35138b16f8875d3c6cf44eb04a777f
Sachin Thakur <thakursachin467@gmail.com>
2017-08-26 00:34:34 +0530

    comments added

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -22,8 +22,8 @@ $(function() {
          * page?
          */
         it('are defined', function() {
-            expect(allFeeds).toBeDefined();
-            expect(allFeeds.length).not.toBe(0);
+            expect(allFeeds).toBeDefined(); //check wether all feeds is defined or not
+            expect(allFeeds.length).not.toBe(0); //check wether all feeds contain some value or not
         });
          });
 
@@ -35,10 +35,10 @@ $(function() {
          */
          it('are defined',function() {
 
-            for(var i=0;i<allFeeds.length;i++)
+            for(var i=0;i<allFeeds.length;i++) //this loop is used to iterate through all feeds 
             {
-            expect(allFeeds[i].url).toBeDefined();
-            expect(allFeeds[i].url.length).not.toBe(0);
+            expect(allFeeds[i].url).toBeDefined(); //here it checks wether the all feeds has url define at all its position 
+            expect(allFeeds[i].url.length).not.toBe(0); //here it checks wether all feeds url has some value by comparing it not to be(0) also !=0 in simple language
           
             }
          });
@@ -53,10 +53,10 @@ $(function() {
          */
          it('are defined',function()
          {
-        for(var i=0;i<allFeeds.length;i++)
+        for(var i=0;i<allFeeds.length;i++)  //this loop is used to iterate through all feeds 
             {
-                expect(allFeeds[i].name).toBeDefined();
-                expect(allFeeds[i].name).not.toBe(0);
+                expect(allFeeds[i].name).toBeDefined();  //here it checks wether the all feeds has name define at all its position 
+                expect(allFeeds[i].name).not.toBe(0);   //here it checks wether all feeds name has some value by comparing it not to be(0) also !=0 in simple language
 
             }
          });
@@ -74,8 +74,8 @@ $(function() {
          * hiding/showing of the menu element.
          */
         it('is hidden',function(){
-                var menu=document.getElementsByClassName('menu-hidden');
-            expect(menu).toBeTruthy();
+                var menu=document.getElementsByClassName('menu-hidden'); //here we use getelementsbyclassname to get the element of menu-hidden into menu
+            expect(menu).toBeTruthy(); //now if menu-hidden is present it means that the menu is hidden
 
 
         });
@@ -84,13 +84,13 @@ $(function() {
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
-         it('MENU CHANGES visibility',function() {
-            $('.menu-icon-link').trigger('click');
-            expect($('body').hasClass('menu-hidden')).toBeFalsy();
+         it('MENU CHANGES visibility',function() { //to check wether menu change actually happen we  have to click the menu icon bar and see the menu hidden property behaviour
+            $('.menu-icon-link').trigger('click');  //here we trigger the click function it opens the nav bar
+            expect($('body').hasClass('menu-hidden')).toBeFalsy(); //now if the nav bar is open then menu-hidden should not be present
 
-            $('.menu-icon-link').trigger('click');
-            expect($('body').hasClass('menu-hidden')).toBeTruthy();
-
+            $('.menu-icon-link').trigger('click');  //now again the trigger function will be called and the menu bar will be closed 
+            expect($('body').hasClass('menu-hidden')).toBeTruthy();  //now due to above click function the class should now be hidden 
+                //now for the menu changes to be visible both the expect condition should pass then only this test case  will be passed
 
           });
 
@@ -101,15 +101,15 @@ $(function() {
     describe('Initial Entries',function(){
 
       beforeEach(function(){
-        loadFeed(0,function(){
-            done();
+        loadFeed(0,function(){ //now here we call the loadfeed with parameter 0 i.e first feed udacity blog
+            done();   //here it passes the control 
         });
 
         
       });
       it('it has atleast one entry',function(){
-        expect($('.feed').length).not.toBe(0);
-
+        expect($('.feed').length).not.toBe(0); //now here we check wether the length of feed is 0 or greater now as loadfeed loaded 0 i.e udacity blog so here the value of feed.length will be 1
+                        //now as the feed.length is 1 and it is not equal to 0 so this test case is pased in case there was no feed i.e nothing waspresent at 0 so this test case would have failed
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
@@ -123,15 +123,15 @@ $(function() {
 });
 
     describe('new feed Selection',function(){
-        var element1;
+        var element1; //here we declare element1 becasuse it will be used globally and not only in beforeEach
         beforeEach(function(){
             loadFeed(0,function() {
 
-               element1 =$(".feed").html();
+               element1 =$(".feed").html();   //here we get the html content of loadfeed 0 i.e udacity blog into element1
                
                 });
-            loadFeed(1,function(){
-                done();
+            loadFeed(1,function(){ //here we load the another feed i.e 1 which is css tricks. we could have given any number 1,2,3 
+                done();  //pass the control forward
 
             });
             
@@ -139,9 +139,10 @@ $(function() {
         });
 
         it('content changed to next feed',function() {
-               var element2 = $(".feed").html();
-                expect(element1).not.toBe(element2);
-            });
+               var element2 = $(".feed").html(); //now as in beforeEach loadFeed(1 we loaded css tricks here we are loading all css tricks into element2 
+                expect(element1).not.toBe(element2); 
+                //now as want to see wether new feed is loaded or not we can do so by comparing the element1 which contain udacity blog and element2 which contain css tricks if they are different it means new feed is loaded
+            }); 
         
 
 
